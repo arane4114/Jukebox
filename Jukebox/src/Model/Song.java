dd312ba5450e7b7bbaa5089ce12d76ec1239d67e
Bryce Hammond <Bryce Hammond@BryceHammond-PC>
2014-10-06 15:45:21 -0700

    Little changes to Song

--- Jukebox/src/Model/Song.java
+++ Jukebox/src/Model/Song.java
@@ -1,16 +1,29 @@
 package Model;
 
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 public class Song {
 	private String fileLocation;
 	private String title;
 	private String artist;
 	private String genre;
+	private int playsToday;
+	
+	private final static int MAX_PLAYS = 5;
+	
+	private List<GregorianCalendar> songPlays;
 	
 	public Song (String fileLocation, String title, String artist, String genre){
 		this.fileLocation = fileLocation;
 		this.title = title;
 		this.artist = artist;
 		this.genre = genre;
+		playsToday = 0;
+		songPlays = new ArrayList<GregorianCalendar>();
 	}
 	
 	public String getFileLocation(){
@@ -30,10 +43,35 @@ public class Song {
 	}
 	
 	public boolean canBePlayedAgainToday(){
-		return true;
+		return playsToday > MAX_PLAYS;
 	}
 	
 	public void incresePlayCount(){
-		
+		playsToday ++;
+	}
+	
+	private boolean sameDay(GregorianCalendar today, GregorianCalendar other) {
+	    return today.get(Calendar.YEAR) == other.get(Calendar.YEAR)
+	        && today.get(Calendar.MONTH) == other.get(Calendar.MONTH)
+	        && today.get(Calendar.DAY_OF_MONTH) == other.get(Calendar.DAY_OF_MONTH);
+	}
+
+	public void play() {
+	    if(playsToday <= MAX_PLAYS)
+	      return;
+	    GregorianCalendar today = new GregorianCalendar();
+	    if (songPlays.size() < MAX_PLAYS) {
+	    	songPlays.add(today);
+	    	playsToday++;
+	    } else {
+	      GregorianCalendar last1 = songPlays.get(songPlays.size() - 1);
+	      GregorianCalendar last2 = songPlays.get(songPlays.size() - 2);
+	      if (sameDay(last1, today) && sameDay(last2, today))
+	        return;
+	      else {
+	    	  songPlays.add(today);
+	    	  playsToday++;
+	      }
+	    }
 	}
 }
