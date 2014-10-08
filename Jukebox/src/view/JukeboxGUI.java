bd7497dc1943efd02dc04d79f4e8a3f6c2554432
Bryce Hammond <Bryce Hammond@BryceHammond-PC>
2014-10-08 12:11:53 -0700

    Formating.

--- Jukebox/src/view/JukeboxGUI.java
+++ Jukebox/src/view/JukeboxGUI.java
@@ -41,7 +41,7 @@ public class JukeboxGUI extends JFrame {
 	}
 
 	public JukeboxGUI() {
-		
+
 		String baseDir = System.getProperty("user.dir")
 				+ System.getProperty("file.separator") + "songfiles"
 				+ System.getProperty("file.separator");
@@ -51,15 +51,17 @@ public class JukeboxGUI extends JFrame {
 		songs = new Songs();
 		songs.addSong(new Song(baseDir + "BlueRidgeMountainMist.mp3",
 				"Blue Ridge Mountain Mist", "Ralph Schuckett", 38));
-		songs.addSong(new Song(baseDir + "DeterminedTumbao.mp3", "Determined Tumbao",
-				"FreePlay Music", 20));
-		songs.addSong(new Song(baseDir + "flute.aif", "Flute", "Sun Microsystems", 5));
-		songs.addSong(new Song(baseDir + "spacemusic.au", "Spacemusic", "Unknown", 6));
+		songs.addSong(new Song(baseDir + "DeterminedTumbao.mp3",
+				"Determined Tumbao", "FreePlay Music", 20));
+		songs.addSong(new Song(baseDir + "flute.aif", "Flute",
+				"Sun Microsystems", 5));
+		songs.addSong(new Song(baseDir + "spacemusic.au", "Spacemusic",
+				"Unknown", 6));
 		songs.addSong(new Song(baseDir + "SwingCheese.mp3", "Swing Cheese",
 				"FreePlay Music", 15));
 		songs.addSong(new Song(baseDir + "tada.wav", "Tada", "Microsoft", 2));
-		songs.addSong(new Song(baseDir + "UntameableFire.mp3", "Untameable Fire",
-				"Pierre Langer", 282));
+		songs.addSong(new Song(baseDir + "UntameableFire.mp3",
+				"Untameable Fire", "Pierre Langer", 282));
 
 		table = new JTable(songs);
 		table.setRowSorter(new TableRowSorter<TableModel>(table.getModel())); // needed
@@ -93,10 +95,13 @@ public class JukeboxGUI extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 
-		JOptionPane.showMessageDialog(new JFrame(),
-				"This GUI shows the behavior of individual songs ( a song can be played 5 times a day and mutiple songs can be queued).\n"	+ 
-						"Through JUnit test we have shown a individual student can only play two songs a day.\n"	+
-								"You are not logged in to a student currently.");
+		JOptionPane
+				.showMessageDialog(
+						new JFrame(),
+						"This GUI shows the behavior of individual songs.\n"
+								+ "( a song can be played 5 times a day and mutiple songs can be queued )\n"
+								+ "Through JUnit testing we have shown a individual student can only play two songs a day.\n"
+								+ "You are not logged in to a student currently.");
 	}
 
 	public static void main(String[] args) {
